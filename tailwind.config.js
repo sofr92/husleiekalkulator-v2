d047d4a8908e8f7e26ad1e41d1e19ec06674d799
sofr92 <129745948+sofr92@users.noreply.github.com>
2025-05-20 21:41:52 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      // Legg inn egne farger eller fonter her hvis du vil
+    },
+  },
+  plugins: [],
+};
